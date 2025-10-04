a891601f312e7b68f2894f19caf592e080bf141d
br04aman <br04aman@example.com>
2025-10-04 22:12:45 +0530

    Fix merge  conflits and remove pip upgrade from requirements

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 # Upgrade pip first
 python -m pip install --upgrade pip
-<<<<<<< HEAD
 # Install gunicorn first
 python -m pip install gunicorn
 # Then install other requirements
 python -m pip install -r requirements.txt
-=======
-# Install requirements with specific pip command
-python -m pip install -r requirements.txt
-# Verify gunicorn installation
-python -m pip install gunicorn
->>>>>>> eca20facb17c1301a63150b875588ef29bd045e9
